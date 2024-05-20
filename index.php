b7feed5f971be676766b7c30d46fdc1e078a7be1
felip-araujo <felipearaujoexecutive@gmail.com>
2024-05-20 16:56:18 -0400

    autenticacao

--- index.php
+++ index.php
@@ -6,9 +6,32 @@ require_once './classes/membros.php';
 require_once './classes/lideranca.php';
 require_once './classes/usuarios_funcoes.php';
 
+// Função de autenticação básica
+function authenticate() {
+    if (!isset($_SERVER['PHP_AUTH_USER'])) {
+        header('WWW-Authenticate: Basic realm="My API"');
+        header('HTTP/1.0 401 Unauthorized');
+        echo json_encode(['status' => 'erro', 'dados' => 'Autenticação requerida.']);
+        exit;
+    } else {
+        $username = $_SERVER['PHP_AUTH_USER'];
+        $password = $_SERVER['PHP_AUTH_PW'];
+
+        // Substitua 'seu_usuario' e 'sua_senha' pelas suas credenciais
+        if ($username !== 'felp' || $password !== '159753') {
+            header('HTTP/1.0 403 Forbidden');
+            echo json_encode(['status' => 'erro', 'dados' => 'Credenciais inválidas.']);
+            exit;
+        }
+    }
+}
+
+// Chama a função de autenticação no início do script
+authenticate();
+
 class Rest
 {
-    public static function open($requisicão)
+    public static function open($requisicao)
     {
         $url = explode('/', $_REQUEST['url']);
         $classe = $url[0];
@@ -36,7 +59,6 @@ class Rest
         }
     }
 }
-
 
 if (isset($_REQUEST)) {
     echo Rest::open($_REQUEST);
