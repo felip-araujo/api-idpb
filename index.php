e3c87ec00e9ce3b2875919c71fa88f737952e02c
felip-araujo <felipearaujoexecutive@gmail.com>
2024-05-28 08:13:05 -0400

    no-auth

--- index.php
+++ index.php
@@ -6,49 +6,6 @@ require_once './classes/membros.php';
 require_once './classes/lideranca.php';
 require_once './classes/usuarios_funcoes.php';
 
-function authenticate()
-{ 
-
-    $username = $_SERVER['PHP_AUTH_USER'] ?? null;
-    $password = $_SERVER['PHP_AUTH_PW'] ?? null ;
-
-    if (!$username || !$password) {
-        header('WWW-Authenticate: Basic realm="My API"');
-        header('HTTP/1.0 401 Unauthorized');
-        echo json_encode(['status' => 'erro', 'dados' => 'Autenticação requerida.']);
-        exit;
-    }
-
-    // if(!isset($_SERVER)){
-    //     header('WWW-Authenticate: Basic realm="My API"');
-    //     header('HTTP/1.0 401 Unauthorized');
-    //     echo json_encode(['status' => 'erro', 'dados' => 'Erro de servidor']);
-    //     exit;
-    // }
-    // // Verifica se os parâmetros de autenticação foram enviados
-    
-    
-
-    // // Adiciona logs para depuração
-    // error_log("Autenticação tentativa de usuário: $username");
-
-    // // Substitua 'seu_usuario' e 'sua_senha' pelas suas credenciais
-    // $valid_user = 'felp';
-    // $valid_pass = '159753';
-
-    // // Verifica as credenciais
-    // if ($username !== $valid_user || $password !== $valid_pass) {
-    //     header('HTTP/1.0 403 Forbidden');
-    //     echo json_encode(['status' => 'erro', 'dados' => 'Credenciais inválidas.']);
-    //     exit;
-    // }
-
-    // // Se as credenciais forem válidas, continue com a execução do script
-    // error_log("Autenticação bem-sucedida para usuário: $username");
-}
-
-// Chama a função de autenticação no início do script
-authenticate();
 
 
 class Rest
